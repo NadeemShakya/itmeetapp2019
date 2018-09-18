6c60f9e79530a6f6d4d758a26ff378b71393983f
Nadeem <shakya.nadim12@gmail.com>
2018-09-18 23:47:33 +0545

    'happycoding'

---
+++ App.js
@@ -0,0 +1,30 @@
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Let's Build Our IT MEET 2019 Application.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#02b875',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#fff'
+  }
+
+});
