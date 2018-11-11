8a7a4a5ff34e9e7aa486c1067589a8c70c34f66d
Nadeem <shakya.nadim12@gmail.com>
2018-11-11 19:12:36 +0545

    changed appregistry to Landing Page.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import Scanner from './src/components/QRCodeScanner';
 // import {name as appName} from './app.json';
 
-AppRegistry.registerComponent("itmeet2019", () => Scanner);
+AppRegistry.registerComponent("itmeet2019", () => App);
