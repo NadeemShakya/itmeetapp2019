edc49032e40edfa1c1e9eebf5818353b1c0f7c63
Nadeem <shakya.nadim12@gmail.com>
2018-11-12 18:25:02 +0545

    use of state to hold the programs information

--- src/components/voting.js
+++ src/components/voting.js
@@ -1,11 +1,57 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Image, TouchableOpacity, TouchableHighlight, ScrollView} from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+    TouchableHighlight,
+    ScrollView,
+    FlatList
+} from 'react-native';
 
 
 export default class Voting extends Component {
+  constructor() {
+      super();
+      this.state = {
+          programs : [
+              {
+                id: '1',
+                title: 'Photography Contest',
+                description: `Lorem Ipsum is simply dummy text ofnting and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`,
+                imageURL: require('../images/events/Photography.png')
+              },
+              {
+                id: '2',  
+                title: 'Github Repo Contest',
+                description: `Lorem Ipsum is simply dummy text ofnting and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`,
+                imageURL: require('../images/events/Github.png')             
+              },
+              {
+                id: '1',
+                title: 'Photography Contest',
+                description: `Lorem Ipsum is simply dummy text ofnting and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`,
+                imageURL: require('../images/events/Photography.png')
+              },
+              {
+                id: '1',
+                title: 'Photography Contest',
+                description: `Lorem Ipsum is simply dummy text ofnting and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`,
+                imageURL: require('../images/events/Photography.png')
+              },
+              {
+                id: '1',
+                title: 'Photography Contest',
+                description: `Lorem Ipsum is simply dummy text ofnting and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`,
+                imageURL: require('../images/events/Photography.png')
+              },                                          
+          ]
+      }
+  }
   render() {
     return (
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
         {/* Header Component Start */}
         <View style = {styles.header}>
             <TouchableHighlight underlayColor = 'transparent' onPress = {() => this.props.navigation.goBack(this.props.navigation.state.key)} style = {styles.backIconImage} >
@@ -16,66 +62,37 @@ export default class Voting extends Component {
             </TouchableHighlight>
             <Text style = {{color: '#fff', fontSize: 22}}>Voting Section</Text>
         </View>
-        <ScrollView>
         {/* Header Component End */}
-        
-          <View style={styles.card}>
-            <View style={styles.insideCard}>
-                <View style={styles.leftCard}>
-                <Image 
-                    style={{width: 100, height: 60, resizeMode: 'contain'}}
-                    source={require('../images/events/Photography.png')}
-                />
-                <Text style={{fontSize: 18, color: '#06525F',textAlign: 'center',marginTop: 8}}>Photography</Text>
-                <Text style={{fontSize: 18, color: '#06525F',textAlign: 'center', marginTop: -8}}>Contest</Text>
-                <View
-                    style={{
-                    borderLeftWidth: 2,
-                    borderLeftColor: 'black',
-                    }}
-                />
-                </View>
-                <View style={styles.middleLine}></View>
-                <View style={styles.rightCard}>
-                
-                <View>
-                    <Text>Lorem Ipsum is simply dummy text ofnting and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</Text>
-                </View>
-                <TouchableOpacity onPress = {() => this.props.navigation.navigate('QRCodeScanner') }>
-                        <View style = {styles.button1}>
-                            <Text style = {styles.texts}>Vote Now</Text>
-                        </View>
-                </TouchableOpacity>
-                </View>
-            </View>
-          </View>
+          <FlatList 
+            data = {this.state.programs}
+            keyExtractor = {(item, index) => item.id }
+            renderItem = {({item}) => 
+                <View style={styles.card}>
+                    <View style={styles.insideCard}>
+                        <View style={styles.leftCard}>
+                        <Image 
+                            style={{width: 100, height: 60, resizeMode: 'contain'}}
+                            source={item.imageURL}
+                        />
+                        <Text style={{fontSize: 16, color: '#06525F',textAlign: 'center',marginTop: 8, lineHeight: 20}}>{item.title}</Text>
 
-          <View style={styles.card}>
-            <View style={styles.insideCard}>
-                <View style={styles.leftCard}>
-                <Image
-                    style={{width: 100, height: 60, resizeMode: 'contain'}}
-                    source={require('../images/events/gaming.png')}
-                />
-                <Text style={{fontSize: 18, color: '#06525F',textAlign: 'center',marginTop: 8}}>Project</Text>
-                <Text style={{fontSize: 18, color: '#06525F',textAlign: 'center', marginTop: -8}}>Competition</Text>
-                
-                </View>
-                <View style={styles.middleLine}></View>
-                <View style={styles.rightCard}>
-                <View>
-                    <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</Text>
-                </View>
-                    <TouchableOpacity onPress = {() => this.props.navigation.navigate('QRCodeScanner') }>
-                        <View style = {styles.button1}>
-                            <Text style = {styles.texts}>Vote Now</Text>
                         </View>
-                    </TouchableOpacity>
+                        {/* <View style={styles.middleLine}></View> */}
+                        <View style={styles.rightCard}>
+                        <View>
+                            <Text>{item.description}</Text>
+                        </View>
+                        <TouchableOpacity onPress = {() => this.props.navigation.navigate('QRCodeScanner') }>
+                                <View style = {styles.voteNowButton}>
+                                    <Text style = {styles.texts}>Vote Now</Text>
+                                </View>
+                        </TouchableOpacity>
+                        </View>
+                    </View>
                 </View>
-            </View>
-          </View>
-        </ScrollView>  
-      </View>
+            }
+          />  
+      </ScrollView>
     );
 }
 }
@@ -86,51 +103,49 @@ const styles = StyleSheet.create({
       backgroundColor: '#f2f2f2',
     },
     card:{
-        marginTop: 20,
+        marginTop: 10,
         marginLeft: '5%',
-        height: 177,
+        marginBottom: 2,
         width: '90%',
         backgroundColor: '#fff',
         shadowColor: '#000',
         elevation: 2,
-        flex:1
+        flex:1,
     },
     insideCard:{
         flex:1,
-        flexDirection: 'row'
+        flexDirection: 'row',
+        alignItems:'center',
+        padding: 10
     },
     leftCard:{
         flex:1,
         alignItems: 'center',
         justifyContent: 'center',
-        padding: 10
+        // padding: 10,
+        borderRightWidth:2,
+        borderColor: '#06525F',
     },
-    middleLine:{
-        flex: 0.1,
-        height: 80,
-        marginTop: '7%',
-        borderLeftWidth:2,
-        borderLeftColor: '#06525F',
 
-    },
     rightCard:{
-        flex:1.9,
-        padding: 10,
-        paddingTop: 20,
+        flex:2,
+        paddingLeft: 15,
+        paddingTop:30,
         alignItems: 'flex-end',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
     },
-    button1: {
-      height: 40,
-      width: 100,
+    voteNowButton: {
+      height: 35,
+      width: 90,
       borderRadius: 5,
       backgroundColor: "#06525F",
       alignItems: 'center',
       padding: 5,
+      marginRight: 10
       },
 
     texts: {
-      fontSize: 18,
+      fontSize: 16,
       color: '#fff'
     },
     header: {
